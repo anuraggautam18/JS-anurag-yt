5f4b53613a96993aeb90e1330eabbaeeec0e569c
Anurag Gautam <157272124+anuraggautam18@users.noreply.github.com>
2024-01-22 12:21:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+" use strict";    // treat all JS code as newer version
+
+// alert(3+3)  we are using nodejs , not browser
+
+let name = "Anurag"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to power 53
+//bigint 
+//string  => ""
+//boolean => true/false
+//null => standalone value
+//undefined => abhi value assign nhi huyi hai 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "anurag");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+//undefined apne app me eak type  hai par null eak object hai
